b9d43a7f3f4914143f5443e220617b1e4a7828e9
Evgeny Kursakov <ekursakov@yandex.ru>
2016-05-17 19:48:22 +0300

    Removed comments from calculator tests

--- modules/fraction-calc/test/test_fraction_calculator.cpp
+++ modules/fraction-calc/test/test_fraction_calculator.cpp
@@ -1,5 +1,3 @@
-// Copyright 2016 Kursakov Evgeny
-
 #include <gtest/gtest.h>
 
 #include <string>
@@ -51,79 +49,58 @@ class FractionCalculatorTest: public ::testing::Test {
 };
 
 TEST_F(FractionCalculatorTest, Do_Print_Help_Without_Arguments) {
-    // Arrange
     vector<string> args = {};
 
-    // Act
     Act(args);
 
-    // Assert
     Assert("This is a fraction calculator application\\..*");
 }
 
 TEST_F(FractionCalculatorTest, Is_Checking_Number_Of_Arguments) {
-    // Arrange
     vector<string> args = {"10/2", "+"};
 
-    // Act
     Act(args);
 
-    // Assert
     Assert("ERROR: Should be 3 arguments\\..*");
 }
 
 TEST_F(FractionCalculatorTest, Can_Add_Fractions) {
-    // Arrange
-    vector<string> args = {"1/4", "+",  "3/8"};
+    vector<string> args = {"1/4", "+", "3/8"};
 
-    // Act
     Act(args);
 
-    // Assert
     Assert("5/8");
 }
 
 TEST_F(FractionCalculatorTest, Can_Diff_Fractions) {
-    // Arrange
-    vector<string> args = {"1/4", "-",  "3/8"};
+    vector<string> args = {"1/4", "-", "3/8"};
 
-    // Act
     Act(args);
 
-    // Assert
     Assert("-1/8");
 }
 
 TEST_F(FractionCalculatorTest, Can_Multiply_Fractions) {
-    // Arrange
-    vector<string> args = {"11/3", "*",  "3/2"};
+    vector<string> args = {"11/3", "*", "3/2"};
 
-    // Act
     Act(args);
 
-    // Assert
     Assert("11/2");
 }
 
 TEST_F(FractionCalculatorTest, Can_Divide_Fractions) {
-    // Arrange
-    vector<string> args = {"8/3", "/",  "2/5"};
+    vector<string> args = {"8/3", "/", "2/5"};
 
-    // Act
     Act(args);
 
-    // Assert
     Assert("20/3");
 }
 
 TEST_F(FractionCalculatorTest, Can_Pass_Fraction_As_Integer) {
-    // Arrange
-    vector<string> args = {"1", "+",  "3/2"};
+    vector<string> args = {"1", "+", "3/2"};
 
-    // Act
     Act(args);
 
-    // Assert
     Assert("5/2");
 }
 
